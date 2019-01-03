e728f1a4c8118493df58f58275032a2b9d99dd61
Joe Waithaka <waitha28ka@gmail.com>
2019-01-04 01:08:57 +0300

    [Start #162971234] Add endpoint for asking questions

--- app/api/v1/views/api_routes.py
+++ app/api/v1/views/api_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 from app.api.v1.models.users_model import UserModel
 from app.api.v1.models.questions_model import QuestionModel
 from app.db import init_quiz_db
+from app.api.v1.utils.utils import requires_token
 
 
 v1 = Blueprint("v1", __name__, url_prefix="/api/v1/")
@@ -56,3 +57,22 @@ def view_answers(id):
         "question_details": quiz.view_answers(id)
     })
 
+
+@requires_token
+@v1.route('/questions', methods=['POST'])
+def ask_question():
+    """Ask a new question endpoint."""
+    data = request.json
+    if data == '' or data is None:
+        return jsonify({
+            "status": "error",
+            "message": "Missing field/s (user_id, question_brief, question_description)"
+        }), 409
+    id = data.get('user_id')
+    question_brief = data.get('question_brief')
+    question_description = data.get('question_description')
+    quiz.ask(id, question_brief, question_description)
+    return jsonify({
+        "status": "success",
+        "message": "Question created successfully!"
+    }), 201
