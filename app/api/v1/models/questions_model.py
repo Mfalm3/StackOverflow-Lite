3b5b6c33c3e6ee1840681bec8dce732effaf376d
Mfalm3 <waitha28ka@gmail.com>
2019-01-03 15:53:29 +0300

    Update questions_model.py

--- app/api/v1/models/questions_model.py
+++ app/api/v1/models/questions_model.py
@@ -5,7 +5,17 @@ from app.db import init_quiz_db
 class QuestionModel:
     """Question Model class."""
 
+    def __init__(self):
+        """Initialize question model."""
+        self.db = init_quiz_db()
+
     def view_questions(self):
         """View all quesitons."""
-        self.db = init_quiz_db()
-        return self.db
+        questions = self.db
+        return questions
+
+    def view_answers(self, id):
+        """View all answers to a question."""
+        answers = self.db
+        quiz_and_answers = answers[id-1]
+        return quiz_and_answers
